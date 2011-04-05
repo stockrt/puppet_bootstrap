a4e138921459a25832171a8508702664b7d4a088
Rogério Carvalho Schneider <stockrt@gmail.com>
2011-04-05 08:59:50 -0300

    exec path

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,5 +3,5 @@ import "roles/*"
 
 # globals
 Exec {
-    path => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin",
+    path => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/opt/bin:/opt/sbin:/opt/local/bin:/opt/local/sbin",
 }
